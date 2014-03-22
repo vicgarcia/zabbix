13152c78b8000c2b651152bbe7b0cd878beefe53
Vic <vic.garcia@bsctechnology.com>
2014-03-21 19:11:20 -0500

    remove install snippets from zagent installer

--- zagent.sh
+++ zagent.sh
@@ -3,6 +3,20 @@
 # zagent.sh - install zabbix agent on a ubuntu system (run this as root)
 #             this script will also configure nginx and php-fpm monitoring
 
+# install zabbix agent
+add-apt-repository ppa:pbardov/zabbix -y && apt-get -qq -y update
+apt-get -y install zabbix-agent
+
+# add zabbix user to the 'adm' group (necessary for log monitoring)
+usermod -a -G adm zabbix
+
+# create location for zabbix monitor scripts
+mkdir -p /etc/zabbix/scripts
+
+#mysql -uroot -p&lt;mysql root password goes here&gt; -e"GRANT USAGE ON *.* TO 'zabbix'@'localhost' IDENTIFIED BY '123456'";
+#mysql -uroot -p&lt;mysql root password goes here&gt; -e"flush privileges"
+#mysql -uzabbix -p123456 -e"status"
+
 # get settings to use to configure the agent from user
 echo -e "What's the IP for the Zabbix server?"
 read ZABBIX_SERVER_IP
@@ -11,92 +25,6 @@ read THIS_SERVER_IP
 echo -e "What's this server's hostname that Zabbix uses?"
 read THIS_SERVER_HOSTNAME
 echo -e ""
-
-# install zabbix agent
-add-apt-repository ppa:pbardov/zabbix -y && apt-get -qq -y update
-apt-get -y install zabbix-agent
-
-# create location for zabbix monitor scripts
-mkdir -p /etc/zabbix/scripts
-
-# install fpm monitoring script
-cat > /etc/zabbix/scripts/phpfpm.sh << DELIM
-#!/bin/bash
-
-ZBX_REQ_DATA="\$1"
-ZBX_REQ_DATA_URL="\$2"
-PHPFPM_STATUS_DEFAULT_URL="http://localhost:80/php-fpm_status"
-WGET_BIN="/usr/bin/wget"
-ERROR_NO_ACCESS_FILE="-0.9900"
-ERROR_NO_ACCESS="-0.9901"
-ERROR_WRONG_PARAM="-0.9902"
-ERROR_DATA="-0.9903" # either can not connect / bad host / bad port
-
-if [ ! -z "\$ZBX_REQ_DATA_URL" ]; then
-    URL="\$ZBX_REQ_DATA_URL"
-else
-    URL="\$PHPFPM_STATUS_DEFAULT_URL"
-fi
-
-PHPFPM_STATS=\$(\$WGET_BIN -q \$URL -O - 2> /dev/null)
-
-if [ \$? -ne 0 -o -z "\$PHPFPM_STATS" ]; then
-    echo \$ERROR_DATA
-    exit 1
-fi
-
-RESULT=\$(echo "\$PHPFPM_STATS" | awk 'match($0, "^'"\$ZBX_REQ_DATA"':[[:space:]]+(.*)", a) { print a[1] }')
-if [ \$? -ne 0 -o -z "\$RESULT" ]; then
-    echo \$ERROR_WRONG_PARAM
-    exit 1
-fi
-
-echo \$RESULT
-
-exit 0
-DELIM
-chmod +x /etc/zabbix/scripts/phpfpm.sh
-
-# install nginx monitoring script
-cat > /etc/zabbix/scripts/nginx.sh << DELIM
-#!/bin/bash
-
-ZBX_REQ_DATA="\$1"
-ZBX_REQ_DATA_URL="\$2"
-NGINX_STATUS_DEFAULT_URL="http://localhost:80/nginx_status"
-WGET_BIN="/usr/bin/wget"
-ERROR_NO_ACCESS_FILE="-0.9900"
-ERROR_NO_ACCESS="-0.9901"
-ERROR_WRONG_PARAM="-0.9902"
-ERROR_DATA="-0.9903" # either can not connect /	bad host / bad port
-
-if [ ! -z "\$ZBX_REQ_DATA_URL" ]; then
-    URL="\$ZBX_REQ_DATA_URL"
-else
-    URL="\$NGINX_STATUS_DEFAULT_URL"
-fi
-
-NGINX_STATS=\$(\$WGET_BIN -q \$URL -O - 2> /dev/null)
-
-if [ \$? -ne 0 -o -z "\$NGINX_STATS" ]; then
-    echo \$ERROR_DATA
-    exit 1
-fi
-
-case \$ZBX_REQ_DATA in
-    active_connections)   echo "\$NGINX_STATS" | head -1             | cut -f3 -d' ';;
-    accepted_connections) echo "\$NGINX_STATS" | grep -Ev '[a-zA-Z]' | cut -f2 -d' ';;
-    handled_connections)  echo "\$NGINX_STATS" | grep -Ev '[a-zA-Z]' | cut -f3 -d' ';;
-    handled_requests)     echo "\$NGINX_STATS" | grep -Ev '[a-zA-Z]' | cut -f4 -d' ';;
-    reading)              echo "\$NGINX_STATS" | tail -1             | cut -f2 -d' ';;
-    writing)              echo "\$NGINX_STATS" | tail -1             | cut -f4 -d' ';;
-    waiting)              echo "\$NGINX_STATS" | tail -1             | cut -f6 -d' ';;
-    *)                    echo \$ERROR_WRONG_PARAM; exit 1;;
-esac
-
-exit 0
-DELIM
-chmod +x /etc/zabbix/scripts/nginx.sh
 
 # add configure zabbix agent
 cat > /etc/zabbix/zabbix_agentd.conf << DELIM
@@ -111,11 +39,7 @@ ListenPort=10050
 Server=$ZABBIX_SERVER_IP
 
 UserParameter=nginx[*],/etc/zabbix/scripts/nginx.sh "\$1" "\$2"
-UserParameter=php-fpm[*],/etc/zabbix/scripts/phpfpm.sh "\$1" "\$2"
 DELIM
-
-# add zabbix user to the 'adm' group (necessary for log monitoring)
-usermod -a -G adm zabbix
 
 # restart zabbix agent with new settings
 service zabbix-agent restart
