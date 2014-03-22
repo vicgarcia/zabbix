540122176a1bb8be7004ccd0ad7d52f2a05410f9
Vic Garcia <vic.garcia@bsctechnology.com>
2014-03-21 20:18:03 -0500

    add curl calls to download/install monitor scripts

--- zagent.sh
+++ zagent.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # zagent.sh - install zabbix agent on a ubuntu system (run this as root)
-#             this script will also configure nginx and php-fpm monitoring
 
 # install zabbix agent
 add-apt-repository ppa:pbardov/zabbix -y && apt-get -qq -y update
@@ -13,9 +12,13 @@ usermod -a -G adm zabbix
 # create location for zabbix monitor scripts
 mkdir -p /etc/zabbix/scripts
 
-#mysql -uroot -p&lt;mysql root password goes here&gt; -e"GRANT USAGE ON *.* TO 'zabbix'@'localhost' IDENTIFIED BY '123456'";
-#mysql -uroot -p&lt;mysql root password goes here&gt; -e"flush privileges"
-#mysql -uzabbix -p123456 -e"status"
+# install redis monitor script from git repo with curl
+curl -o /etc/zabbix/scripts/monitor-redis.pl https://raw.githubusercontent.com/vicgarcia/zabbix-scripts/master/monitor-redis.pl
+chmod +x /etc/zabbix/scripts/monitor-redis.pl
+
+# install nginx monitor script from git repo with curl
+curl -o /etc/zabbix/scripts/monitor-nginx.sh https://raw.githubusercontent.com/vicgarcia/zabbix-scripts/master/monitor-nginx.sh
+chmod +x /etc/zabbix/scripts/monitor-nginx.sh
 
 # get settings to use to configure the agent from user
 echo -e "What's the IP for the Zabbix server?"
@@ -40,6 +43,8 @@ Server=$ZABBIX_SERVER_IP
 
 UserParameter=nginx[*],/etc/zabbix/scripts/monitor-nginx.sh "\$1" "\$2"
 UserParameter=redis_stats[*],/etc/zabbix/scripts/monitor-redis.pl "\$1" "\$2" "\$3"
+
+Include=/etc/zabbix/zabbix_agentd.conf.d/
 DELIM
 
 # restart zabbix agent with new settings
